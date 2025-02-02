2f796e88bc0e2d5710a17e5b51bc6107dc0933c6
Md Murad Hossain <muradmd312@gmail.com>
2025-02-02 15:59:19 +0600

    img added for basic component

---
+++ src/component/Hero.jsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+    return (
+        <div>
+            <img src="https://uat-brcp.oss.net.bd/home/images/products/profile/6530aa0a4f387_istockphoto-1303938818-612x612.jpg" />
+        </div>
+    )
+}
+
+export default Hero;
